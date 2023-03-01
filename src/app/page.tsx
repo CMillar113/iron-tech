c43bdd701e903b768fd20ffc5997da44cabcff4d
Odhran McGranaghan <odhran.19@gmail.com>
2023-03-01 22:38:22 +0000

    Change dir structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get started by EDITING&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
